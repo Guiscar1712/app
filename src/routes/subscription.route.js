6b5ae3e108818552792a000f7305c437d4aa32d8
Ricardo Sampaio <ricardo.sampaio@bluecore.it>
2023-01-13 11:02:28 -0300

    courses

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const SubscriptionController = require('../controllers/subscription.controller')
 const TrackMiddleware = require('../middlewares/trackMiddleware')
 
+router.get('/courses', TrackMiddleware.tracking, SubscriptionController.courses)
+
 router.get('/:cpf', TrackMiddleware.tracking, SubscriptionController.get)
 router.post('/', TrackMiddleware.tracking, SubscriptionController.post)
 
