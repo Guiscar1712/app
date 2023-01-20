8fa70c7916f09431582acaac2af75809a99cbce3
Hercules E Santos <hercules.eduardo@gmail.com>
2023-01-20 01:56:36 -0300

    feat: implementing get course by identifier

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -5,6 +5,7 @@ const SubscriptionController = require('../controllers/subscription.controller')
 const TrackMiddleware = require('../middlewares/trackMiddleware')
 
 router.get('/courses', TrackMiddleware.tracking, SubscriptionController.courses)
+router.get('/courses/:identifier', TrackMiddleware.tracking, SubscriptionController.getCourse)
 
 router.get('/:cpf', TrackMiddleware.tracking, SubscriptionController.get)
 router.post('/', TrackMiddleware.tracking, SubscriptionController.post)
