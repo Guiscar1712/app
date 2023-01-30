dd1e5ef4bd37d22c51a6fc492921b79c3f0b192d
Heitor F Neto <heitorfernandesnt@gmail.com>
2023-01-30 11:05:19 -0300

    voltando endpoint courses em subscription

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const CourseController = require('../controllers/course.controller')
 const SubscriptionController = require('../controllers/subscription.controller')
 const TrackMiddleware = require('../middlewares/trackMiddleware')
 
 router.get('/:cpf', TrackMiddleware.tracking, SubscriptionController.get)
 router.post('/', TrackMiddleware.tracking, SubscriptionController.post)
+
+router.get('/courses', TrackMiddleware.tracking, CourseController.courses)
+router.get('/courses/:identifier', TrackMiddleware.tracking, CourseController.getCourse)
 
 module.exports = router
