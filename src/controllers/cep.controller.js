fede52e46756790204ddb7a6d39a60cdee3e254a
Hercules E Santos <hercules.eduardo@gmail.com>
2023-05-17 15:51:54 -0300

    feat: implementing cep integration

---
+++ src/controllers/cep.controller.js
@@ -0,0 +1,14 @@
+const CepService = require('../services/cep.service')
+
+module.exports = class CepController {
+  static async getInfoAddressByCep (request, response, next) {
+    try {
+      const cep = request.params.cep
+      const info = await CepService.getInfoAddressByCep(cep)
+      response.json(info)
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  }
+}
