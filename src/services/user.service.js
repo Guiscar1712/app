91793963aeba65ccc31b8a22e5e8f168914778cf
Heitor F Neto <heitorfernandesnt@gmail.com>
2023-06-27 00:59:23 -0300

    errors message custom

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,6 +9,7 @@ const Util = require('../utils/util')
 const AzureService = require('./azure.service.js')
 const { encryptPassword, comparePassword, getRecoverKey } = require('../utils/auth')
 const { firebaseAdmin } = require('../services/firebase.service')
+const { DuplicateRegister } = require('../validators/user')
 
 module.exports = class UserService {
   static async findById (id) {
@@ -61,53 +62,35 @@ module.exports = class UserService {
     return model
   }
 
-  static async validateEntity (user, entity) {
-    if (!entity.cpf) {
-      throw new Error('cpf não informado!')
-    }
-
-    if (!entity.email) {
-      throw new Error('email não informado!')
-    }
-
-    entity.cpf = Util.getNumbers(entity.cpf)
-    this.duplicateRegister(
-      user,
-      await UserRepository.findBy({ CPF: entity.cpf }),
-      'CPF já cadastrado!'
-    )
-
-    if (entity.email) {
-      this.duplicateRegister(
-        user,
-        await UserRepository.findBy({ Email: entity.email }),
-        'Email já cadastrado!'
-      )
-    }
-
-    entity.phone = Util.getNumbers(entity.phone)
-    if (entity.phone) {
-      this.duplicateRegister(
-        user,
-        await UserRepository.findBy({ Phone: entity.phone }),
-        'Phone já cadastrado!'
-      )
-    }
-  }
-
   static async register (entity) {
-    // proteção para não atualizar outro registro
     if (entity.id) {
       entity.id = null
     }
 
-    await this.validateEntity(null, entity)
+    const contract = await this.validateEntity(entity)
+    if (!contract.isValid()) {
+      return { errors: contract.errors() }
+    }
 
     const user = await this.createdUser(entity)
 
     await this.sendCodeEmail(user.id, user.name, entity.email)
 
     return user
+  }
+
+  static async validateEntity (entity) {
+    entity.cpf = Util.getNumbers(entity.cpf)
+    entity.phone = Util.getNumbers(entity.phone)
+
+    const search = await UserRepository.findByOr({ CPF: entity.cpf }, { Email: entity.email }, { Phone: entity.phone })
+
+    const user = {}
+    user.cpf = search.find(f => f.cpf === entity.cpf)?.cpf
+    user.email = search.find(f => f.email === entity.email)?.email
+    user.phone = search.find(f => f.phone === entity.phone)?.phone
+
+    return DuplicateRegister(user)
   }
 
   static async createdUser (entity) {
@@ -273,7 +256,10 @@ module.exports = class UserService {
       throw new Error('usuário não logado!')
     }
 
-    await this.validateEntity(user, entity)
+    const contract = await this.validateUpdateEntity(userId, entity)
+    if (!contract.isValid()) {
+      return { errors: contract.errors() }
+    }
 
     let updateEntity = this.getModel(entity)
 
@@ -291,6 +277,20 @@ module.exports = class UserService {
 
     updateEntity = this.prepare(updateEntity)
     return await UserRepository.update(user.id, updateEntity)
+  }
+
+  static async validateUpdateEntity (userId, entity) {
+    entity.cpf = Util.getNumbers(entity.cpf)
+    entity.phone = Util.getNumbers(entity.phone)
+
+    const search = await UserRepository.findByOr({ CPF: entity.cpf }, { Email: entity.email }, { Phone: entity.phone })
+
+    const user = {}
+    user.cpf = search.find(f => f.cpf === entity.cpf && f.id !== userId)?.cpf
+    user.email = search.find(f => f.email === entity.email && f.id !== userId)?.email
+    user.phone = search.find(f => f.phone === entity.phone && f.id !== userId)?.phone
+
+    return DuplicateRegister(user)
   }
 
   static async recoverPassword (cpf) {
---
+++ src/validators/user/index.js
@@ -0,0 +1,23 @@
+const Validator = require('../validator')
+
+module.exports = {
+  ApplyValidate (model) {
+    const contract = new Validator()
+
+    contract.isRequired(model.cpf, '40001', 'CPF é Obrigatório')
+    contract.isFixedLen(model.cpf, 11, '40002', 'CPF inválido')
+    contract.isRequired(model.name, '40003', 'Nome é Obrigatório')
+    contract.isEmail(model.email, '40004', 'E-mail inválido')
+
+    return contract
+  },
+
+  DuplicateRegister (model) {
+    const contract = new Validator()
+    contract.duplicateRegister(model.cpf, '40005', 'CPF já cadastrado!')
+    contract.duplicateRegister(model.email, '40006', 'E-mail já cadastrado!')
+    contract.duplicateRegister(model.phone, '40007', 'Telefone já cadastrado!')
+
+    return contract
+  }
+}
