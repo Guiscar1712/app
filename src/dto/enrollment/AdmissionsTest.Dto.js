6e9596fedbac0fac5d304f1de712e5e5ee87226a
Heitor F Neto <heitorfernandesnt@gmail.com>
2023-07-03 09:44:09 -0300

    refactored subscriptions search endpoint

---
+++ src/dto/enrollment/AdmissionsTest.Dto.js
@@ -0,0 +1,9 @@
+class AdmissionsTest {
+  constructor (data) {
+    this.active = data.active
+    this.approved = data.approved
+    this.elegibility = data.elegibility
+  }
+}
+
+module.exports = AdmissionsTest
