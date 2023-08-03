9bfee03bef9b34ad90e5b1524c2abf69c998541f
Heitor Fernandes Neto <heitorfneto@Heitors-iMac-Pro.local>
2023-08-03 11:47:38 -0300

    fix duration log

--- src/model/logger/Log.js
+++ src/model/logger/Log.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 const Message = require('./Message')
 class Log {
   constructor (data) {
-    this.entryDate = moment().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
+    this.entryDate = moment().format('YYYY-MM-DD HH:mm:ss.SSS')
     this.service = data.service
     this.thread = data.thread
     this.SetLevelINFO()
@@ -21,8 +21,9 @@ class Log {
   }
 
   Finalize (data) {
+    const entryDate = moment(this.entryDate)
     const endDate = moment()
-    this.duration = moment.duration(endDate.diff(this.entryDate)).asMilliseconds()
+    this.duration = moment.duration(endDate.diff(entryDate)).asMilliseconds()
   }
 }
 
--- src/model/logger/Step.js
+++ src/model/logger/Step.js
@@ -2,13 +2,15 @@ const moment = require('moment')
 const obscureSensitiveData = require('./../../extensions/obscureSensitiveData')
 class Step {
   constructor () {
-    this.entryDate = moment().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
+    this.entryDate = moment().format('YYYY-MM-DD HH:mm:ss.SSS')
   }
 
   finalize (data) {
     this.data = obscureSensitiveData(data)
+
+    const entryDate = moment(this.entryDate)
     const endDate = moment()
-    this.duration = moment.duration(endDate.diff(this.entryDate)).asMilliseconds()
+    this.duration = moment.duration(endDate.diff(entryDate)).asMilliseconds()
   }
 }
 
