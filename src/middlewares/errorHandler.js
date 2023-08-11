40972c094013226ea179a81a0de8585cc575aa0c
Heitor Fernandes Neto <heitorfernandesnt@gmail.com>
2023-08-11 15:32:53 -0300

    ErrorHandler type

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,35 +1,20 @@
-const { NotFoundError, ClientServerError } = require('../utils/errors')
-const ValidationError = require('../utils/errors/ValidationError')
+const BaseError = require('../utils/errors/BaseError')
+
 const logger = require('../utils/logger.util')
 
 const errorHandler = (error, req, res, next) => {
   logger.error(JSON.stringify(error, Object.getOwnPropertyNames(error)))
 
-  if (error instanceof ValidationError) {
-    res.status(error.errorCode).json({
-      type: error.errorType,
-      errorCode: error.errorCode,
-      message: error.message,
-      errors: error.serializeErrors()
-    })
-  }
+  const send = {}
 
-  if (error instanceof NotFoundError) {
-    res.status(error.errorCode).json({
-      type: error.errorType,
-      errorCode: error.errorCode,
+  if (error instanceof BaseError) {
+    send.error = {
+      type: error.type,
+      errorCode: error.code,
       message: error.message,
       errors: error.serializeErrors()
-    })
-  }
-
-  if (error instanceof ClientServerError) {
-    res.status(error.errorCode).json({
-      type: error.errorType,
-      errorCode: error.errorCode,
-      message: error.message,
-      errors: error.serializeErrors()
-    })
+    }
+    return res.status(error.statusCode).json(send)
   }
 
   res.status(500).send('Something went wrong')
