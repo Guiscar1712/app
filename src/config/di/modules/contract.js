4c39956b06c08e7ba0f38689e7fa76f1aab5758b
jair.alves <jair.alves@bluecore.it>
2023-08-24 14:51:58 -0300

    add - contract Server

--- src/config/di/modules/contract.js
+++ src/config/di/modules/contract.js
@@ -1,8 +1,9 @@
 const { asClass } = require('awilix')
 
 const ContractController = require('../../../controllers/v1/contract.controller')
+const ContractService = require('../../../services/enrollments/contractAccepted.js')
 
 module.exports = {
-
-  ContractController: asClass(ContractController).scoped()
+  ContractController: asClass(ContractController).scoped(),
+  ContractService: asClass(ContractService).scoped()
 }
