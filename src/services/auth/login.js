0bb0000f3db9488cfe4e1c977711f29a0fa98998
Heitor F Neto <heitorfernandesnt@gmail.com>
2023-11-30 16:55:17 -0300

    fix validation

--- src/services/auth/login.js
+++ src/services/auth/login.js
@@ -3,15 +3,22 @@ const constantUser = require(`../../constants/user.constants`)
 const constantAuth = require(`../../constants/auth.constants`)
 const jwt = require('jsonwebtoken')
 const config = require('../../utils/config')
+const encrypedCipher = require('./encrypedCipher')
 
 module.exports = class AuthLoginService {
-  constructor({ UserRepository, MembershipRepository, LoggerService }) {
+  constructor({
+    UserRepository,
+    MembershipRepository,
+    VerificadorTokenValidar,
+    LoggerService,
+  }) {
     this.UserRepository = UserRepository
-    this.MembershipRepository = MembershipRepository
+    ;(this.MembershipRepository = MembershipRepository),
+      (this.VerificadorTokenValidar = VerificadorTokenValidar)
     this.LoggerService = LoggerService
   }
 
-  request = async (provider, userId, key) => {
+  request = async (provider, receiver, userId, key) => {
     const step = this.LoggerService.addStep('AuthLoginService')
     try {
       const user = await this.UserRepository.findBy({ id: userId })
@@ -19,19 +26,20 @@ module.exports = class AuthLoginService {
         throw new NotFoundError(
           `Registro não encontrado`,
           [constantUser.NOT_FOUND],
-          constantAuth.code
+          constantAuth.CODE
         )
       }
 
       const membership = await this.MembershipRepository.findBy({
         UserId: user.id,
       })
-      if (key !== membership.recoveryKey) {
-        throw new ValidationError(
-          `Parâmetros inválidos`,
-          [constantAuth.INVALID_KEY],
-          constantAuth.code
-        )
+
+      if (provider === 'verification-code') {
+        this.verificationCode(key, membership)
+      }
+
+      if (provider === 'verification-external') {
+        await this.verificationExternal(user, key)
       }
 
       await this.MembershipRepository.update(membership.id, {
@@ -46,5 +54,37 @@ module.exports = class AuthLoginService {
     } finally {
       this.LoggerService.finalizeStep(step)
     }
+  }
+
+  verificationCode(key, membership) {
+    if (key !== membership.recoveryKey) {
+      throw new ValidationError(
+        `Parâmetros inválidos`,
+        [constantAuth.INVALID_KEY],
+        constantAuth.CODE
+      )
+    }
+  }
+
+  async verificationExternal(receiver, key, document) {
+    let vericadorBody = {}
+    if (reciver === 'sms') {
+      vericadorBody.sistema = 1
+    } else {
+      throw new ValidationError(
+        `Parâmetros inválidos`,
+        [constantAuth.INVALID_KEY],
+        constantAuth.CODE
+      )
+    }
+
+    const data = {
+      identificador: document,
+      codigo: key,
+    }
+
+    vericadorBody.token = encrypedCipher(data)
+
+    await this.VerificadorTokenValidar.request(vericadorBody)
   }
 }
